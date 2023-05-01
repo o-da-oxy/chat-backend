e7b7a9a8ea918560c62c5f642712772beb0c714e
Oksana <demskaya01@gmail.com>
2023-05-01 16:43:07 +0300

    update rooms dataset, fix socket

--- src/index.ts
+++ src/index.ts
@@ -70,7 +70,6 @@ io.on('connection', (socket: Socket) => {
       const messages = await getLastMessagesFromRoom(room);
       const roomMessages = await groupMessagesByDate(messages);
       io.to(room).emit('room-messages', roomMessages);
-      socket.emit('room-messages', roomMessages);
     }
   );
 
